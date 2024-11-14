7edb4b6b9be5bca0f29a3243d63808449af3d388
John Borg <john.borg237@gmail.com>
2024-11-14 08:24:36 -0600

    working local and server

--- netlify/functions/fetchTokens.js
+++ netlify/functions/fetchTokens.js
@@ -9,11 +9,19 @@ exports.handler = async (event) => {
     const data = await response.json();
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allows access from any origin
+        'Access-Control-Allow-Headers': 'Content-Type', // Allows specific headers
+      },
       body: JSON.stringify(data),
     };
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Ensures CORS headers are included in error responses
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
       body: JSON.stringify({ error: error.message }),
     };
   }
